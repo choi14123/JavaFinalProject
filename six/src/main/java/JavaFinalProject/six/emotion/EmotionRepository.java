766b2cdabb089d957701a565f391b417728d5b9b
jhs5122 <141017514+jhs5122@users.noreply.github.com>
2025-06-09 04:56:43 +0900

    Delete six/src/main/java/JavaFinalProject/six/emotion directory

--- six/src/main/java/JavaFinalProject/six/emotion/EmotionRepository.java
+++
@@ -1,7 +0,0 @@
-package JavaFinalProject.six.emotion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmotionRepository extends JpaRepository<Emotion, Long> {
-    Emotion findByName(String emotion);
-}
